96aa81f9f47c06a495dedc6d2a7f8fb8457d05d1
Beyza Yurukakar <beyzayurukakar.w@gmail.com>
2024-10-29 17:43:37 +0300

    feat: exporting necessary values from index.ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,8 @@
+import { slice } from './store/slice';
+
+export const inAppLaunchSliceName = slice.name;
+export const inAppLaunchReducer = slice.reducer;
 export { default as listenerMwTools } from './api/listenerMwTools';
 export { useInAppLaunch } from './api/useInAppLaunch';
 export { useJobAtLaunch } from './api/useJobAtLaunch';
+export { useIsLaunchComplete } from './api/useIsLaunchComplete';
