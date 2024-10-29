bd35c4f4078cec8534bc0035df77ecde0bb37fc3
Beyza Yurukakar <beyzayurukakar.w@gmail.com>
2024-10-29 17:33:57 +0300

    feat: initial setup in example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,30 +1,12 @@
-import { useState, useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-in-app-launch';
+import Home from './components/home/Home';
+import Launch from './components/launch/Launch';
 
 export default function App() {
-    const [result, setResult] = useState<number | undefined>();
+    const isLaunchComplete = false;
 
-    useEffect(() => {
-        multiply(3, 7).then(setResult);
-    }, []);
+    if (isLaunchComplete) {
+        return <Home />;
+    }
 
-    return (
-        <View style={styles.container}>
-            <Text>Result: {result}</Text>
-        </View>
-    );
+    return <Launch />;
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    box: {
-        width: 60,
-        height: 60,
-        marginVertical: 20,
-    },
-});
---
+++ example/src/components/launch/Launch.tsx
@@ -0,0 +1,7 @@
+import { Text } from 'react-native';
+
+const Launch = () => {
+    return <Text>Launch</Text>;
+};
+
+export default Launch;
