ec8a37251033f4308621b794c2702c6eced27722
Beyza Yurukakar <beyzayurukakar.w@gmail.com>
2024-10-29 16:46:36 +0300

    feat: added hook for managing launch cycle and renamed files

--- src/api/useInAppLaunch.ts
+++ src/api/useInAppLaunch.ts
@@ -1,52 +1,19 @@
-import React, { useCallback, useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectors } from '../store/selectors';
+import { useEffect } from 'react';
 import { slice } from '../store/slice';
 
-export const useInAppLaunchEffect = (effect: React.EffectCallback, ...jobNames: string[]) => {
-    const areJobsCompleted = useSelector((state: any) =>
-        selectors.isJobArrCompleted(state, jobNames)
-    );
-    const effectRef = useRef(effect);
-
+export const useInAppLaunch = ({ isAnimationComplete }: { isAnimationComplete: boolean }) => {
     const dispatch = useDispatch();
+    const areAllJobsDone = useSelector(selectors.areAllJobsDone);
 
     useEffect(() => {
-        /*
-        Using an up-to-date reference of `effect`.
-        Not using `effect` directly as deps in the next useEffect
-        because we want to call it only when `areJobsCompleted` has changed, not when `effect` has changed
-        */
-        effectRef.current = effect;
-    }, [effect]);
+        dispatch(slice.actions.initialize());
+    }, [dispatch]);
 
     useEffect(() => {
-        let unsubscribe: ReturnType<React.EffectCallback>;
-        if (areJobsCompleted) {
-            unsubscribe = effectRef.current();
+        if (areAllJobsDone && isAnimationComplete) {
+            dispatch(slice.actions.completeInAppLaunch());
         }
-
-        return () => {
-            unsubscribe?.();
-        };
-    }, [areJobsCompleted]);
-
-    const addToPendingJobs = useCallback(
-        (jobName: string) => {
-            dispatch(slice.actions.addToPendingJobs(jobName));
-        },
-        [dispatch]
-    );
-
-    const removeFromPendingJobs = useCallback(
-        (jobName: string) => {
-            dispatch(slice.actions.removeFromPendingJobs(jobName));
-        },
-        [dispatch]
-    );
-
-    return {
-        addToPendingJobs,
-        removeFromPendingJobs,
-    };
+    }, [dispatch, areAllJobsDone, isAnimationComplete]);
 };
---
+++ src/api/useJobAtLaunch.ts
@@ -0,0 +1,52 @@
+import React, { useCallback, useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectors } from '../store/selectors';
+import { slice } from '../store/slice';
+
+export const useJobAtLaunch = (effect: React.EffectCallback, ...jobNames: string[]) => {
+    const areJobsCompleted = useSelector((state: any) =>
+        selectors.isJobArrCompleted(state, jobNames)
+    );
+    const effectRef = useRef(effect);
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        /*
+        Using an up-to-date reference of `effect`.
+        Not using `effect` directly as deps in the next useEffect
+        because we want to call it only when `areJobsCompleted` has changed, not when `effect` has changed
+        */
+        effectRef.current = effect;
+    }, [effect]);
+
+    useEffect(() => {
+        let unsubscribe: ReturnType<React.EffectCallback>;
+        if (areJobsCompleted) {
+            unsubscribe = effectRef.current();
+        }
+
+        return () => {
+            unsubscribe?.();
+        };
+    }, [areJobsCompleted]);
+
+    const addToPendingJobs = useCallback(
+        (jobName: string) => {
+            dispatch(slice.actions.addToPendingJobs(jobName));
+        },
+        [dispatch]
+    );
+
+    const removeFromPendingJobs = useCallback(
+        (jobName: string) => {
+            dispatch(slice.actions.removeFromPendingJobs(jobName));
+        },
+        [dispatch]
+    );
+
+    return {
+        addToPendingJobs,
+        removeFromPendingJobs,
+    };
+};
