9b4506455c70b5def6b8cb3dddcfef05df9b487b
Beyza Yurukakar <beyzayurukakar.w@gmail.com>
2024-11-12 16:01:24 +0300

    refactor: renaming exported variables

--- example/src/components/launch/JobBox.tsx
+++ example/src/components/launch/JobBox.tsx
@@ -1,13 +1,15 @@
 import { ActivityIndicator, Dimensions, StyleSheet, Text, View } from 'react-native';
 import { JOB_DEPS, JOB_MWS, type JOB_NAMES } from '../../jobs/constants';
 import { useSelector } from 'react-redux';
-import { selectors } from 'react-native-in-app-launch';
+import { inAppLaunchSelectors } from 'react-native-in-app-launch';
 import type { RootState } from '../../store/configuration';
 
 const JobBox = (props: { jobName: keyof typeof JOB_NAMES }) => {
     const { jobName } = props;
 
-    const jobStatus = useSelector((state: RootState) => selectors.jobStatus(state, jobName));
+    const jobStatus = useSelector((state: RootState) =>
+        inAppLaunchSelectors.jobStatus(state, jobName)
+    );
 
     const renderJobStatus = () => {
         if (jobStatus === undefined) {
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 export * from './tools';
 export { sliceExternal as inAppLaunchSlice } from './store/slice';
-export { selectors } from './store/selectors';
+export { selectors as inAppLaunchSelectors } from './store/selectors';
 export { registerListeners as registerInAppLaunchListeners } from './store/listeners';
